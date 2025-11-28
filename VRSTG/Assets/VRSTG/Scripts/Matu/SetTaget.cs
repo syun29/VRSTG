2e4326a8a20da18628b7f9c64731cfe54ca37ff0
camuchasu <24051908@d.anabuki-college.net>
2025-11-28 11:28:06 +0900

    Scene wake

--- VRSTG/Assets/VRSTG/Scripts/Matu/SetTaget.cs
+++ VRSTG/Assets/VRSTG/Scripts/Matu/SetTaget.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class SetTarget : MonoBehaviour
 {
     [SerializeField] float m_target;
+    //[SerializeField] 
 
     // Update is called once per frame
     void Update()
